409241e4f74b669ced012c6c045500503aad34dd
lufanta <45773201+lufanta@users.noreply.github.com>
2018-12-11 20:05:07 +0000

    Add files via upload

--- 1.py
+++ 1.py
@@ -9,29 +9,48 @@
 # robot.left() # Eskubiko motorrak segi eta ezkerrekoa geratu
 
 
+#
+#import Rpi.GPIO as GPIO
+#from time import sleep 
+
+#GPIO.setupmode(GPIO.BOARD)
+
+#GPIO.setup(portua1, GIPO.out(), initial = 1) # Portua1 Output gisa sortzen aktibatuta
+#GPIO.setup(portua2, GIPO.out(), initial = 0) # Portua2 Output gisa sortzen aktibatu gabe
+
+#try:
+ #  while True:
+ #      GPIO.output(portua1, 1) # portua1 eko Output/a aktibatu
+  #     sleep(5.0) # 5 segundu itxarongo ditu
+   #    GPIO.output(portua1, 0)# portua1 eko Output/a desaktibatu
+#except KeyboardInterrupt:
+ #   GPIO.cleanup()
+
+
 import Rpi.GPIO as GPIO
-from time import sleep 
+from time import sleep
 
 GPIO.setupmode(GPIO.BOARD)
 
-GPIO.setup(portua1, GIPO.out(), initial = 1) # Portua1 Output gisa sortzen aktibatuta
-GPIO.setup(portua2, GIPO.out(), initial = 0) # Portua2 Output gisa sortzen aktibatu gabe
+GPIO.setup(18, GPIO.OUT)# PWM-rako erabilgarriak diren bakarrak 18, 12, 13 19
+GPIO.setup(12, GPIO.OUT)
 
+m1 = GPIO.pwm(18, 40) #Motorraren portua eta bere frekuentzia jartzen. GPIO.PWM(Portua, frekuentzia)
+m2 = GPIO.pwn(12, 40)
+motorrak[m1, m2]
+
+
+# motorren "duty cicle" = 50. %50 eko potentziarekin = 3.3V / 2
+motorrak.start(50)
 
 try:
-   while True:
-       GPIO.output(portua1, 1) # portua1 eko Output/a aktibatu
-       sleep(5.0) # 5 segundu itxarongo ditu
-       GPIO.output(portua1, 0)# portua1 eko Output/a desaktibatu
-       while i >= 10:
-         GPIO.output(portua1, 1)
-         time = 1
-         sleep(time)
-         time += 1
-         i += 1
-         #10 aldiz mugituko du robota. ALdi bakoitzean segundu bat gehiago itxaroten
-         
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    while x != 100:
+        x = 75
+        motorrak.ChangeDutyCicle(x) # Motorren "duty cicle" = 75. %75 eko potentzian = 3.3V * 0,75
+        x += 1 # Motorren "duty cicle"-a ziklo bakoitzena 1 igoko da. Beraz, abiadura gero eta azkarrago
+
 finally:
-   GPIO.cleanup()
+    cleanup()
+
+try:
+    while True:
