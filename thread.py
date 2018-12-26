a593ba90d6639afce3d26d65a584292b7cbf1aa7
lufanta <45773201+lufanta@users.noreply.github.com>
2018-12-26 16:26:12 +0100

    Create thread.py

---
+++ thread.py
@@ -0,0 +1,28 @@
+import random
+import time
+from threading import Thread
+
+
+class WaitForUserInput(Thread):
+    def run(self):
+        # your original code
+        a = random.randint(0, 9)
+        ans = 2 * a
+        q = ""
+        q = int(input("Calculate 2 * %d\n" % a))
+        # added these lines of code from my side :)
+        if q == ans:
+            print('You did it!')
+
+
+def print_message():
+    print("You can do it")
+    time.sleep(3)
+
+
+while True:
+    waiting_for_input = WaitForUserInput()
+    waiting_for_input.start()
+
+    while waiting_for_input.is_alive():
+        print_message()
