25b3c799ae08a4e34a272b2fad4de8032cd5c2d4
lufanta <45773201+lufanta@users.noreply.github.com>
2019-01-08 12:04:03 +0100

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,31 +1,51 @@
 import random
 import time
 from threading import Thread
-from GPIO import RPI.gpio as RPI
+from RPi.GPIO import GPIO as GPIO
 
-RPI.
+GPIO.setmode(GPIO.BOARD)
 
-
-class WaitForUserInput(Thread):
-    def run(self):
-        # your original code
-        a = random.randint(0, 9)
-        ans = 2 * a
-        q = ""
-        q = int(input("Calculate 2 * %d\n" % a))
-        # added these lines of code from my side :)
-        if q == ans:
-            print('You did it!')
+GPIO.setup(gorri, GPIO.OUT, initial = 0)
+GPIO.setup(hori, GPIO.OUT, initial = 0)
+GPIO.setup(urdin, GPIO.OUT, initial = 0)
 
 
-def print_message():
-    print("You can do it")
-    time.sleep(3)
+gorri = input(int("Gorriaren tokia")
+hori = input(int("horiaren tokia")
+urdin = input(int("urdinaren tokia")
 
+koloreak [gorri, hori,urdin]
 
+class WaitForUserInput(Thread, koloreak):
+    def run(self,koloreak):
+        #Main on Thread
+        GPIO.output(gorri, 1)
+        sleep(1)
+        GPIO.output(gorri, 0)
+        GPIO.output(urdin, 1)
+        sleep(1)
+        GPIO.output(urdin, 0)
+        GPIO.output(hori, 1)
+        sleep(1)
+
+def main(koloreak):
+         number1 = random.randint(0, 9)
+         number2 = random.randint(0, 9)
+         result = number1 * number2
+         erantzuna = int(input("Zenbat da {} * {}").value(number1, number1)))
+         if erantzuna == result:
+             GPIO.output(urdina, 1)
+             sleep(2)
+             GPIO.output(urdina, 0)
+        else:
+             GPIO.output(gorri, 1)
+             sleep(2)
+             GPIO.output(gorri, 0)
+              
+              
 while True:
     waiting_for_input = WaitForUserInput()
     waiting_for_input.start()
 
     while waiting_for_input.is_alive():
-        print_message()
+        main()
